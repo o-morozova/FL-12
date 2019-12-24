f4f085eccd5ecb890d5a36ba3516d03e71e54f01
o-morozova <whatinside@gmail.com>
2019-12-24 21:53:47 +0200

    Implemented app for identifying triangle type.

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -4,14 +4,14 @@ let b = Number(prompt('Enter value for b', ''));
 let c = Number(prompt('Enter value for c', ''));
 if (a===0 || isNaN(a) || isNaN(b) || isNaN(c)) {
     alert('Invalid input data');
-} else{
+} else {
     let D = b * b - 4 * a * c;
     if(D < 0) {
         console.log('no solution');
     } else if (D === 0) {
         let x = - (b / (2 * a));
         console.log('x=' + Math.round(x));
-    } else{
+    } else {
         let x1 = (-b + Math.sqrt(D)) / (2 * a);
         let x2 = (-b - Math.sqrt(D)) / (2 * a);
         console.log('x1=' + Math.round(x1));
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,2 +1,19 @@
 'use strict';
-
+let a = parseInt(prompt('Enter value for side a', ''));
+let b = parseInt(prompt('Enter value for b', ''));
+let c = parseInt(prompt('Enter value for c', ''));
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    alert('input values should be ONLY numbers');
+} else if (a===0 || b===0 || c===0) {
+    alert('A triangle must have 3 sides with a positive definite length');
+} else if (!(a + b > c) || !(b + c > a) || !(a + c > b)) {
+    alert('Triangle doesn\'t exist');
+} else {
+    if (a===b && b===c && c===a) {
+        console.log('Equilateral triangle');
+    } else if (a === b || b ===c || c===a) {
+        console.log('Isosceles triangle');
+    } else {
+        console.log('Scalene triangle');
+    }
+}
