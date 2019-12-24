101ea914ccc5509e832f482a27d4d49c017a78a0
o-morozova <whatinside@gmail.com>
2019-12-24 16:06:55 +0200

    Implemented app for solving Quadratic equation.

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,20 @@
-// Your code goes here
+'use strict';
+let a = Number(prompt('Enter value for a', ''));
+let b = Number(prompt('Enter value for b', ''));
+let c = Number(prompt('Enter value for c', ''));
+if (a===0){
+    alert('Invalid input data');
+} else{
+    let D = b * b - 4 * a * c;
+    if(D < 0) {
+        console.log('no solution');
+    } else if (D === 0) {
+        let x = - (b / (2 * a));
+        console.log('x=' + Math.round(x));
+    } else{
+        let x1 = (-b + Math.sqrt(D)) / (2 * a);
+        let x2 = (-b - Math.sqrt(D)) / (2 * a);
+        console.log('x1=' + Math.round(x1));
+        console.log('x2=' + Math.round(x2));
+    }
+}
