0f222fb2bfba5d818768a55de5f6f0c1559bb3d2
o-morozova <whatinside@gmail.com>
2019-12-31 16:25:53 +0200

    pipe is ready

---
+++ FL12_HW8/homework/js/pipe.js
@@ -0,0 +1,12 @@
+function addOne(x) {
+    return x + 1;
+}
+function pipe() {
+    let number = arguments[0];
+    for (let i = 1; i < arguments.length; i++) {
+        number = arguments[i](number);
+    }
+    return number;
+}
+console.log(pipe(1, addOne));
+console.log(pipe(1, addOne, addOne));
