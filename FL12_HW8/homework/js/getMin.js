b253f585fe2083ee656d966cc60858b9d6d3edcb
o-morozova <whatinside@gmail.com>
2019-12-31 13:04:58 +0200

    getMin is ready

---
+++ FL12_HW8/homework/js/getMin.js
@@ -0,0 +1,14 @@
+function getMin() {
+    // arguments - array with all arguments of the function
+    if (arguments.length === 0) {
+        return 'Function requires at least one argument'; // verifying that function has something to work with
+    }
+    let min = arguments[0];
+    for (let argumentsIndex = 1; argumentsIndex < arguments.length; ++argumentsIndex) {
+        if (arguments[argumentsIndex] < min) {
+            min = arguments[argumentsIndex];
+        }
+    }
+    return min;
+}
+console.log(getMin(3,0,-3));
