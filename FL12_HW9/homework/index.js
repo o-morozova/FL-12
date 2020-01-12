9ea317e475b5bba17a4ab36ad2e9446729a12dae
o-morozova <whatinside@gmail.com>
2020-01-12 18:57:58 +0200

    executeforEach function is ready

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,11 +1,21 @@
 function convert() {
-    console.log (arguments.length);
     let conversionResults = [];
     for (let i = 0; i < arguments.length; i++) {
-        (typeof arguments[i] === 'string') ?
+        typeof arguments[i] === 'string' ?
             conversionResults.push(parseInt(arguments[i])) :
             conversionResults.push('' + arguments[i]);
         }
     return conversionResults;
 }
-console.log (convert('1', 2, 3, '4'));
+console.log(convert('1', 2, 3, '4'));
+
+function executeforEach() {
+    let arrayToProcess = arguments[0];
+    let functionForElements = arguments[1];
+    for (let el = 0; el < arrayToProcess.length; el++) {
+        functionForElements(arrayToProcess[el]);
+    }
+}
+executeforEach([1,2,3], function(el) {
+    console.log(el * 2)
+});
