d49c071e77ffed8b32c189acbc51c4eb82480644
o-morozova <whatinside@gmail.com>
2020-01-12 12:14:08 +0200

    convert function is ready

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+function convert() {
+    console.log (arguments.length);
+    let conversionResults = [];
+    for (let i = 0; i < arguments.length; i++) {
+        (typeof arguments[i] === 'string') ?
+            conversionResults.push(parseInt(arguments[i])) :
+            conversionResults.push('' + arguments[i]);
+        }
+    return conversionResults;
+}
+console.log (convert('1', 2, 3, '4'));
