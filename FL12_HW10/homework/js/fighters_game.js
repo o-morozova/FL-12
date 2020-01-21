921f41830aa064a08b0ee0a7c6ecc59c9c4f59f9
o-morozova <whatinside@gmail.com>
2020-01-22 00:20:00 +0200

    refactored to hide props

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,71 +1,83 @@
 const maxProbability = 100;
-class Fighter {
-    constructor(){
-        this._name = arguments[0].name;
-        this._damage = arguments[0].damage;
-        this._totalHp = arguments[0].hp;
-        this._strength = arguments[0].strength;
-        this._agility = arguments[0].agility;
-        this._currentHp = this._totalHp;
-        this._wins = 0;
-        this._losses = 0;
-    }
-    getName() {
-        return this._name;
-    }
-    getDamage() {
-        return this._damage;
-    }
-    getStrength() {
-        return this._strength;
-    }
-    getAgility() {
-        return this._agility;
-    }
-    getHealth() {
-        return this._currentHp;
-    }
-    attack(defender) {
-        if (!(defender instanceof Fighter)) {
-            console.log('Error. This function requires an object of the Fighter class');
-        } else {
+function Fighter({name, damage, hp, strength, agility}){
+    let fighter = {name:  name,
+                    damage: damage,
+                    hp: hp,
+                    strength: strength,
+                    agility: agility,
+                    wins: 0,
+                    losses: 0,
+                    currentHp: hp};
+    return {
+        getMaxHp: function () {
+            return fighter.hp;
+        },
+        getName: function () {
+            return fighter.name;
+        },
+        getDamage: function () {
+            return fighter.damage;
+        },
+        getStrength: function () {
+            return fighter.strength;
+        },
+        getAgility: function () {
+            return fighter.agility;
+        },
+        getHealth: function () {
+            return fighter.currentHp;
+        },
+        getWins: function () {
+            return fighter.wins;
+        },
+        getLosses: function () {
+            return fighter.losses;
+        },
+        setHealth: function (health){
+            fighter.currentHp = health;
+        },
+        setWins: function (newWins){
+            fighter.wins = newWins;
+        },
+        setLosses: function (newLoss) {
+            fighter.losses = newLoss;
+        },
+        attack: function (defender) {
             let probabilityOfSuccessfulAttack = maxProbability - (defender.getStrength() + defender.getAgility());
             let randomNumber = Math.round(Math.random() * maxProbability);
             if (randomNumber < probabilityOfSuccessfulAttack) {
-                defender.dealDamage(this._damage);
-                console.log(this._name + ' makes ' + this._damage + ' damage to ' + defender.getName());
+                defender.dealDamage(this.getDamage());
+                console.log(this.getName() + ' makes ' + this.getDamage() + ' damage to ' + defender.getName());
             } else {
-                console.log(this._name + ' attack missed');
+                console.log(this.getName() + ' attack missed');
             }
+        },
+        logCombatHistory: function () {
+            console.log('Name: ' + this.getName(), 'Wins: ' + this.getWins(), 'Losses: ' + this.getLosses());
+        },
+        heal: function (amountOfHealthPoints) {
+            this.setHealth(this.getHealth() + amountOfHealthPoints);
+            if (this.getHealth() > this.getMaxHp()) {
+                this.setHealth(this.getMaxHp());
+            }
+        },
+        dealDamage: function (amountOfHealthPoints) {
+            this.setHealth(this.getHealth() - amountOfHealthPoints);
+            if (this.getHealth() < 0) {
+                this.setHealth(0);
+            }
+        },
+        addWin: function () {
+            this.setWins(this.getWins() + 1);
+        },
+        addLoss: function () {
+            this.setLosses(this.getLosses() +1);
         }
-    }
-    logCombatHistory() {
-        console.log('Name: ' + this._name, 'Wins: ' + this._wins, 'Losses: ' + this._losses);
-    }
-    heal(amountOfHealthPoints) {
-        this._currentHp += amountOfHealthPoints;
-        if (this._currentHp > this._totalHp) {
-            this._currentHp = this._totalHp;
-        }
-    }
-    dealDamage(amountOfHealthPoints) {
-        this._currentHp -= amountOfHealthPoints;
-        if (this._currentHp < 0) {
-            this._currentHp = 0;
-        }
-    }
-    addWin() {
-        this._wins += 1;
-    }
-    addLoss() {
-        this._losses += 1;
     }
 }
 
 function battle(myFighterObject, myFighterObject2) {
-    if (!(myFighterObject instanceof Fighter) || !(myFighterObject2 instanceof Fighter)){
-        console.log('Error. This function requires 2 objects of the Fighter class');
-    } else if (myFighterObject.getHealth() === 0) {
+   if (myFighterObject.getHealth() === 0) {
         console.log(`${myFighterObject.getName()} is dead and can't fight`);
     } else if (myFighterObject2.getHealth() === 0) {
         console.log(`${myFighterObject2.getName()} is dead and can't fight`);
@@ -88,7 +100,3 @@ function battle(myFighterObject, myFighterObject2) {
         }
     }
 }
-
-const myFighterObject = new Fighter({name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25});
-const myFighterObject2 = new Fighter({name: 'Commodus', damage: 21, hp: 95, strength: 40, agility: 35});
-
